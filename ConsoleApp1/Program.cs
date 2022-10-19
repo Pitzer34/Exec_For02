116d55d72896afd4996bfab480e5b3dd655108f2
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 14:42:57 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //等腰星形三角形
+            for(int i = 1; i <= 5; i++)
+            {
+                int space = 5 - i;
+                int leftstar = i;
+                int rightstar = i - 1;
+
+                string row = new string(' ', space)+new string('*', leftstar)+new string('*', rightstar);
+                Console.WriteLine(row);
+            }
+
+
+
+        }
+    }
+}
